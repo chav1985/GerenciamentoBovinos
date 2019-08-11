72ef13dd7c25dcfc8c533d600cee0852dfec0328
carlos henrique <carlos.henrique1985@hotmail.com>
2019-08-11 13:08:35 -0300

    Alteração do localDB e campo descrição que retirou required na classe bovino

--- GerenciamentoBovinos/Models/Bovino.cs
+++ GerenciamentoBovinos/Models/Bovino.cs
@@ -31,8 +31,6 @@ namespace GerenciamentoBovinos.Models
         [Display(Name = "Vlr Unitário")]
         public decimal VlrUnitario { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
-        [MinLength(3, ErrorMessage = "A {0} deve ter no mínimo {1} caracteres.")]
         [MaxLength(200, ErrorMessage = "A {0} deve ter no máximo {1} caracteres")]
         [Display(Name = "Descrição")]
         public string Descricao { get; set; }
