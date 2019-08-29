1ed712c570c865912a914a7444de9b394baa50f8
Bruno Anjos <bruno.c.anjos@gmail.com>
2019-08-28 21:14:44 -0300

    CRUD de Cliente

--- GerenciamentoBovinos/Models/Cliente.cs
+++ GerenciamentoBovinos/Models/Cliente.cs
@@ -2,13 +2,14 @@
 
 namespace GerenciamentoBovinos.Models
 {
-    public class Veterinario : Pessoa
+    public class Cliente : Pessoa
     {
         [Key]
         public long Id { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
         [MaxLength(30, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
-        public string CRMV { get; set; }
+        [Display(Name = "Inscrição Estadual")]
+        public string InscricaoEstadual { get; set; }
+
     }
 }
--- GerenciamentoBovinos/Models/Pessoa.cs
+++ GerenciamentoBovinos/Models/Pessoa.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace GerenciamentoBovinos.Models
 {
     public class Pessoa
     {
-        [Key]
-        public long Id { get; set; }
-
         [Required(ErrorMessage = "Campo obrigatório")]
         [MinLength(3, ErrorMessage = "O {0} deve ter no mínimo {1} caracteres.")]
         [MaxLength(50, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
