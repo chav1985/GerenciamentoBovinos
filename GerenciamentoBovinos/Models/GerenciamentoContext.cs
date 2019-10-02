791d3b1c1bc641182f93472315e970dd898a3cc3
unicesumar <wendel.santos@unicesumar.edu.br>
2019-10-01 21:09:48 -0300

    Corrigido a herança

--- GerenciamentoBovinos/Models/GerenciamentoContext.cs
+++ GerenciamentoBovinos/Models/GerenciamentoContext.cs
@@ -5,7 +5,7 @@ namespace GerenciamentoBovinos.Models
 {
     public class GerenciamentoContext : DbContext
     {
-        public GerenciamentoContext() : base("name=DbBoi10")
+        public GerenciamentoContext() : base("name=DbBoi11")
         {
 
         }
